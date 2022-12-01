c5de1bd4e4acaff44fe7aab21ce7b0dd3287eaf6
Swati Shashi <swati.shashi@ibm.com>
2022-12-01 18:03:11 +0530

    updated Exception class

--- src/main/java/com/example/digitnumeralsconverterapi/controller/ConverterController.java
+++ src/main/java/com/example/digitnumeralsconverterapi/controller/ConverterController.java
@@ -1,5 +1,6 @@
 package com.example.digitnumeralsconverterapi.controller;
 
+import com.example.digitnumeralsconverterapi.exception.ConvertedNumberFormatException;
 import com.example.digitnumeralsconverterapi.util.ConverterUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +11,11 @@ public class ConverterController {
     private ConverterUtil converterUtil;
 
     @RequestMapping(value = "/digitToNumerals", method = { RequestMethod.GET, RequestMethod.POST })
-    public String digitConverter(@RequestParam(value = "digit", defaultValue = "9") Integer digit) {
+    public String digitConverter(@RequestParam(value = "digit", defaultValue = "-1") Integer digit) {
 
         if (digit < 1 || digit > 4000) {
             System.out.println("Invalid roman number value!");
-            throw new NumberFormatException("Value of RomanNumeral must be positive and less than or equal to 4000.");
+            throw new ConvertedNumberFormatException();
         }
 
         String numeral = converterUtil.convertToNumerals(digit);
@@ -27,7 +28,7 @@ public class ConverterController {
         int digit = converterUtil.convertToDigit(numeral);
         if(digit < 1 || digit > 4000){
             System.out.println("Invalid roman number value!");
-            throw new NumberFormatException("Value of RomanNumeral must be positive and less than or equal to 4000.");
+            throw new ConvertedNumberFormatException();
         }
         return digit;
     }
